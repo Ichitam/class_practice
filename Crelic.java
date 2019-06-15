aa439b5b4c0c23f868fa3353533bd716db171836
Ichita_m <nspthifir@gmail.com>
2019-06-15 16:51:32 +0900

    first commit

---
+++ Crelic.java
@@ -0,0 +1,34 @@
+// package Cleric ;
+public class Crelic{
+  String name;
+  int HP = 50;
+  final int maxHP = 50;
+  int MP = 10;
+  final int maxMP = 10;
+
+public Crelic(String name, int HP, int MP) {
+  this.name = name;
+  this.HP = HP;
+  this.MP = MP;
+}
+public Crelic(String name, int HP) {
+  this.name = name;
+  this.HP = HP;
+}
+
+public void selfAid() {
+  this.MP -= 5;
+  this.HP = this.maxHP;
+}
+public int pray(int time){
+  int firstMP = this.MP;
+  int praytime = new java.util.Random().nextInt(3);
+  int recover = time + praytime;
+  this.MP = this.MP + recover;
+  if (this.MP >= maxMP) {
+    this.MP = maxMP;
+  }
+  int output = this.MP - firstMP;
+  return output;
+}
+}
---
+++ main.java
@@ -0,0 +1,12 @@
+// import Cleric ;
+public class main{
+  public static void main(String[] args) {
+    // Crelic asaka = new Crelic("あさか",40,5);
+    // Crelic asaka = new Crelic("あさか",35);
+    Crelic asaka = new Crelic();
+
+    System.out.println(asaka.name);
+    System.out.println(asaka.MP);
+    System.out.println(asaka.HP);
+}
+}
