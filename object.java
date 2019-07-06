93e222a84cc2862f1cac232c7b52142e97c6020f
生田　真深 <mafutan@icloud.com>
2019-07-06 14:01:12 +0900

    a

--- object.java
+++ object.java
@@ -1,34 +1,10 @@
 public class Main {
- public static void main(String[] args){
-//Moneyクラス
-  public class Money{
-  public static void add
-  public static void diff
-  public static void multiple
-  public static void devide
-  a.amount =
-  b.currency =
-  //Factory
-  int a = Factory(c)
-
-  }
-//Factoryクラス
-  public class Factory{
-  public static void makeproduct
-  public static void 
-  c.assembly_cost =
-  //makeproductメソッドの値をMoneyメソッドに戻す
-  public static int makeproduct() {
-  return c
-  }
-  }
-//Partsクラス
-　public class Parts{
-  public static void parts
-  d.assembly_cost =
-  public static int () {
-  return c
-
-}
+ public static void main(String[] args) {
+   int a = 6.7 * 1.1 * 6 * 1000000;
+   int b = 4000 * 2;
+   int c = 5400 * 1.1 * 5;
+   int d = 30 * 3 * 1000000;
+   int e = 30 * 3 * 1000000;
+   System.out.println(a + b + c + d + e)
  }
 }
