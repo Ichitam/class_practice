34cbdd80dab90d55d1e871410e5ad1ecfca7bd1d
生田　真深 <mafutan@icloud.com>
2019-07-06 15:01:43 +0900

    a

--- object.java
+++ object.java
@@ -1,24 +1,28 @@
 public class object {
  public static void main(String[] args) {
-   double mat_a = 67;
-   double mat_b = 30;
-   double plus = 1.1;
-   double assembly_a = 4000;
-   double assembly_b = 30;
-   double assembly_all = 30;
-   double conversion = 100;
-   double quantity_a = 6;
-   double quantity_b = 5;
-   double time_a = 2;
-   double time_b = 3;
-   double time_all = 3;
-
-   double a = mat_a * plus * quantity_a * conversion;
-   double b = assembly_a * time_a;
-   double c = mat_b * plus * quantity_b;
-   double d = assembly_b * time_b * conversion;
-   double e = assembly_all * time_all * conversion;
-
-   System.out.println(a + b + c + d + e);
+   double f = a.mat * all.plus * a.quantity * all.conversion;
+   double g = a.assembly * a.time;
+   double c = b.mat * all.plus * b.quantity;
+   double d = b.assembly * b.time * all.conversion;
+   double e = all.assembly * all.time * all.conversion;
+   System.out.println(f + g + c + d + e);
  }
+}
+public class a {
+  static double mat = 67;
+  static double assembly = 4000;
+  static double quantity = 6;
+  static double time = 2;
+}
+public class b {
+  static double mat = 30;
+  static double assembly = 30;
+  static double quantity = 5;
+  static double time = 3;
+}
+public class all {
+  static double assembly =30;
+  static double time = 3;
+  static double conversion =100;
+  static double plus = 1.1;
 }
