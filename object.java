a6687603f5b308dcb324611cc49246916e091869
生田　真深 <mafutan@icloud.com>
2019-07-06 16:24:52 +0900

    a

--- object.java
+++ object.java
@@ -1,24 +1,28 @@
 public class object {
  public static void main(String[] args) {
-   double f = a.mat * all.plus * a.quantity * all.conversion;
-   double g = a.assembly * a.time;
-   double c = b.mat * all.plus * b.quantity;
-   double d = b.assembly * b.time * all.conversion;
+   double f = h.mat * all.plus * h.quantity * all.conversion;
+   double g = h.assembly * h.time;
+   double c = i.mat * all.plus * i.quantity;
+   double d = i.assembly * i.time * all.conversion;
    double e = all.assembly * all.time * all.conversion;
    System.out.println(f + g + c + d + e);
  }
+ //インスタンス作成
+ a h = new a(67,4000,6,2);
+ a i = new a(30,30,5,3);
 }
+
 public class a {
-  static double mat = 67;
-  static double assembly = 4000;
-  static double quantity = 6;
-  static double time = 2;
-}
-public class b {
-  static double mat = 30;
-  static double assembly = 30;
-  static double quantity = 5;
-  static double time = 3;
+   double mat;
+   double assembly;
+   double quantity;
+   double time;
+   a(double mat,double assembly,double quantity,double time){
+     this.mat = mat;
+     this.assembly = assembly;
+     this.quantity = quantity
+     this.time = time;
+   }
 }
 public class all {
   static double assembly =30;
@@ -26,3 +30,4 @@ public class all {
   static double conversion =100;
   static double plus = 1.1;
 }
+public class
