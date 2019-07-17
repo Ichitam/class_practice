a2159eba7a01d9359629bfc506f612716fdf3ac6
生田　真深 <mafutan@icloud.com>
2019-07-17 21:32:44 +0900

    first commit

--- main.java
+++ main.java
@@ -3,9 +3,10 @@ public class main{
   public static void main(String[] args) {
     // ここでcrelic(名前　あさかHP、４０、MP １０)
     Crelic asaka = new Crelic("あさか", 40 ,10);
-    
-  System.out.println(asaka.name);   System.out.println(asaka.MP);
+  selfAid();
+  System.out.println(asaka.name);
   System.out.println(asaka.HP);
-  System.out.println(asaka.HP);
+  System.out.println(asaka.MP);
+
 }
 }
