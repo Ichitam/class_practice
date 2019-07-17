d20c712044b8ff61646c1ab15b779d0a25e9be85
生田　真深 <mafutan@icloud.com>
2019-07-17 20:47:08 +0900

    first commit

--- main.java
+++ main.java
@@ -1,12 +1,13 @@
 // import Cleric ;
 public class main{
   public static void main(String[] args) {
-    // Crelic asaka = new Crelic("あさか",40,5);
-    // Crelic asaka = new Crelic("あさか",35);
-    Crelic asaka = new Crelic();
-
-    System.out.println(asaka.name);
-    System.out.println(asaka.MP);
-    System.out.println(asaka.HP);
+    // ここでcrelic(名前　あさかHP、４０、MP １０)
+    name a = new name();
+    a.name = "あさか"
+    a.HP = 40;
+    a.MP = 10;
+  System.out.println(asaka.name);   System.out.println(asaka.MP);
+  SSystem.out.println(asaka.HP);
+}}  System.out.println(asaka.HP);
 }
 }
