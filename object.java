ad7355dad644f01ec8ecb4d6617aba13b68f34bb
生田　真深 <mafutan@icloud.com>
2019-07-17 20:07:07 +0900

    first commit

--- object.java
+++ object.java
@@ -1,68 +1,40 @@
-public class object {
+public class object extend Money{
  public static void main(String[] args) {
-   double f = h.mat * h.quantity * all.conversion;
-   double g = h.assembly * h.quantity;
-   double c = i.mat * all.plus * i.quantity;
-   double d = i.assembly * i.quantity * all.conversion;
-   double e = all.assembly  * all.conversion;
-   System.out.println(f + g + c + d + e);
- }
- //インスタンス作成
- a h = new a(67,4000,6,2);
- a i = new a(30,30,5,3);
-}
+   double assemblyAmerica = 30;
+   double assemblyJapan = 4000;
+   double timeA = 2;
+   double timeB = 3;
+   double timeC = 2;
+   double timeOne = 3;
+   double timeTwo = 4;
+   double conversion1 = 100;
+   double conversion2 = 1000000;
+   double plus = 1.1;
+   double plusTwo = 0.1;
+   double product1 = 1;
+   double product2 = 4;
+   //インスタンス作成
+   Part oneA = new Part(67,4000,6,2);
+   Part oneB = new Part(5400,30,5,3);
+   double costOneA = oneA.mat * oneA.quantity * conversion1;
+   double assemblyOneA = oneA.assembly * oneA.quantity * timeA;
+   double costOneB = oneB.mat * plus * oneB.quantity;
+   double assemblyOneB= oneB.assembly * oneB.quantity * conversion1 * timeB;
+   double allCostOne = assemblyAmerica * conversion1 * product1 * timeOne;
+   double plusCost = (costOneA + assemblyOneA + costOneB +assemblyOneB) * plus;
+   // System.out.println(allCostOne + plusCost);
+     //インスタンス作成
+     Part twoB = new Part(5400,30,4,3);
+     Part twoC = new Part(0.007,4000,6,2);
+     double costTwoB = twoB.mat * plus * twoB.quantity;
+     double assemblyTwoB = twoB.assembly * twoB.quantity * timeB * conversion1;
+     double costTwoC = twoC.mat * conversion2 * twoC.quantity;
+     double assemblyTwoC = twoC.assembly * twoC.quantity * timeC;
 
-public class a {
-   double mat;
-   double assembly;
-   double quantity;
-   double time;
-   a(double mat,double assembly,double quantity,double time){
-     this.mat = mat;
-     this.assembly = assembly;
-     this.quantity = quantity;
-     this.time = time;
-   }
-}
-public class all {
-  static double assembly =30;
-  static double time = 3;
-  static double conversion =100;
-  static double plus = 1.1;
-}
+     double allCostTwoJapan = assemblyJapan * (product2 / 2) * timeTwo;
+     double allCostTwoAmerica = assemblyAmerica * conversion1 * (product2 / 2) * timeTwo;
 
-
-//問２！
-public class object {
- public static void main(String[] args) {
-   double f = h.mat * all.plus * h.quantity ;
-   double g = h.assembly * h.quantity;
-   double c = i.mat * all.conversion * i.quantity;
-   double d = i.assembly * i.quantity;
-   double e = all.product * all.average;
-   System.out.println(f + g + c + d + e);
- }
- //インスタンス作成
- a h = new a(5400,30,4,3);
- a i = new a(0.007,4000,6,2);
-}
-
-public class a {
-   double mat;
-   double assembly;
-   double quantity;
-   double time;
-   a(double mat,double assembly,double quantity,double time){
-     this.mat = mat;
-     this.assembly = assembly;
-     this.quantity = quantity;
-     this.time = time;
-   }
-}
-public class all {
-  static double assembly =3500;
-  static double time = 4;
-  static double conversion =1000000;
-  static double plus = 1.1;
-  static double average = 3500;
-  static double product =
+     double plusCost2 = (costTwoB + assemblyTwoB + costTwoC + assemblyTwoC)* (product2 / 2) * plusTwo;
+     System.out.println(allCostTwoJapan + allCostTwoAmerica + ((assemblyTwoB + assemblyTwoC + costTwoB + costTwoC) * product2)+ plusCost2);
+    }
+  }
